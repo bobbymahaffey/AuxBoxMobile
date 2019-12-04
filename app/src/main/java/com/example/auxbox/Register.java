e9c49cb5acba666c9fc5b079e8afd5bfa8580298
Bobby Mahaffey <bobby.mahaffey@oit.edu>
2019-12-04 01:50:28 -0800

    can now store user data in to firebase cloud store under users

--- app/src/main/java/com/example/auxbox/Register.java
+++ app/src/main/java/com/example/auxbox/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +15,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity{
+    public static final String TAG = "TAG";
     EditText mFullName,mEmail,mPassword,mPhone;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+
+
+    String userID;
 
 
 
@@ -41,6 +54,7 @@ public class Register extends AppCompatActivity{
         mLoginBtn    = findViewById(R.id.loginRedirect);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
         //user already logged in or return user
@@ -55,8 +69,12 @@ public class Register extends AppCompatActivity{
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+
+                //retrieve the name and number for database
+                final String fullName = mFullName.getText().toString();
+                final String phone = mPhone.getText().toString();
 
                 //check if empty
                 if(TextUtils.isEmpty(email)){
@@ -84,6 +102,34 @@ public class Register extends AppCompatActivity{
                         //the process of registering the user is called a task
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "user was created", Toast.LENGTH_SHORT).show();
+
+                            //save the ID of the current user
+                            userID = fAuth.getCurrentUser().getUid();
+
+                            //created a collect in the database called users
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+
+                            //store the data into the document using hash map
+                            Map<String,Object> user = new HashMap<>();
+
+                            //insert data into the hash map
+                            // the key is the fname and email then the objects are the fullNmae, email
+                            user.put("fName", fullName);
+                            user.put("email", email);
+                            user.put("phone", phone);
+
+                            //insert the data to the database
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: Message for the logcat user Profile is created for "+ userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
                             //redirect user to main activity
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         }else{
