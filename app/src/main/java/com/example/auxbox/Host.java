2811f12f4f3a6c64a335bb809afac9ca8cde2282
Andrew Tran <andrew.tran@oit.edu>
2019-12-07 13:41:26 -0800

    stop song function

--- app/src/main/java/com/example/auxbox/Host.java
+++ app/src/main/java/com/example/auxbox/Host.java
@@ -11,45 +11,62 @@ package com.example.auxbox;
 
 public class Host extends AppCompatActivity {
 
-    Button mplaySongBtn;
-
+    Button mPlayBtn;
+    Button mStopBtn;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
 
-        mplaySongBtn = findViewById(R.id.playSOngbtn);
-        mplaySongBtn.setOnClickListener(new View.OnClickListener() {
+        mPlayBtn = findViewById(R.id.playButton);
+        mStopBtn = findViewById(R.id.stopButton);
+
+        mediaPlayer = new MediaPlayer();
+        mPlayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                playSong(v);
-
+                playSong(v, mediaPlayer);
             }
         });
+        mStopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                stopSong(v, mediaPlayer);
+            }
+        });
     }
 
-    public void playSong(View v)
+    public void playSong(View v, MediaPlayer m)
     {
-        MediaPlayer mediaPlayer = new MediaPlayer();
         try
         {
-            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/auxbox-29cc0.appspot.com/o/youtubnow.co%20-%20Maroon%205%20-%20Memories.mp3?alt=media&token=6d69241e-1da8-47e1-afe9-8439e966493a");
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            m.setDataSource("https://firebasestorage.googleapis.com/v0/b/auxbox-29cc0.appspot.com/o/youtubnow.co%20-%20Maroon%205%20-%20Memories.mp3?alt=media&token=6d69241e-1da8-47e1-afe9-8439e966493a");
+            m.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                 @Override
                 public void onPrepared(MediaPlayer mp) {
                     mp.start();
                 }
             });
 
-            mediaPlayer.prepare();
+            m.prepare();
         }catch(IOException e)
         {
             e.printStackTrace();
         }
+    }
 
+    public void stopSong(View v, MediaPlayer m)
+    {
+            m.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.stop();
+                }
+            });
     }
 }
 
